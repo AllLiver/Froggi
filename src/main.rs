bb9a10f0377026347ce7284c9652bf4740f4e8ca
AllLiver <97857498+AllLiver@users.noreply.github.com>
2024-02-02 16:41:51 -0700

    Simple frontend-backend interation

--- src/main.rs
+++ src/main.rs
@@ -2,29 +2,30 @@ use axum::{
     body::Body,
     http::Response,
     response::{Html, IntoResponse},
-    routing::get,
+    routing::{get, post},
     Router,
 };
 use hyper::header::CONTENT_TYPE;
 use mime::TEXT_CSS;
 use mime::TEXT_JAVASCRIPT;
 
-const ADDR: &'static str = "127.0.0.1:8080";
+const ADDR: &'static str = "127.0.0.1:8080"; // Sets the address to listen on
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new()
-        .route("/", get(idx_handler))
-        .route("/style.css", get(css_handler))
-        .route("/htmx.min.js", get(htmx_handler));
+    let app = Router::new() // Creates a new router
+        .route("/", get(idx_handler)) // Handles get requests for the index of the app
+        .route("/style.css", get(css_handler)) // Handles get requests for the css of the app
+        .route("/htmx.min.js", get(htmx_handler))
+        .route("/hu", post(hu_handler)); // Handles get requests for the htmx library
 
     println!("Listening on: {}\n", ADDR);
-    let listener = tokio::net::TcpListener::bind(ADDR).await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+    let listener = tokio::net::TcpListener::bind(ADDR).await.unwrap(); // Binds the listener to the address
+    axum::serve(listener, app).await.unwrap(); // Serves the app
 }
 
 async fn idx_handler() -> Html<&'static str> {
-    Html(include_str!("html/index.html"))
+    Html(include_str!("html/index.html")) // Serves the contents of index.html
 }
 
 async fn css_handler() -> impl IntoResponse {
@@ -45,4 +46,9 @@ async fn htmx_handler() -> impl IntoResponse {
         .header(CONTENT_TYPE, TEXT_JAVASCRIPT.to_string())
         .body(body)
         .unwrap()
+}
+
+
+async fn hu_handler() {
+    println!("Home point up");
 }
