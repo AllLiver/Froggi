517e81d93485deed9bb286e09bfc29fcd3001016
AllLiver <97857498+AllLiver@users.noreply.github.com>
2024-02-04 17:49:45 -0700

    Documentation overhaul

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+// Brings the axum backend into scope
 use axum::{
     body::Body,
     extract::Multipart,
@@ -7,20 +8,25 @@ use axum::{
     Form, Router,
 };
 
+// Brings libraries needed for global variables into scope
 use lazy_static::lazy_static;
 use std::{path::Path, sync::Mutex};
 
+// Brings libraries needed for the server headers into scope
 use hyper::{header::CONTENT_TYPE, StatusCode};
 use mime::IMAGE_PNG;
 use mime::TEXT_CSS;
 use mime::TEXT_JAVASCRIPT;
 
+// Brings libraries needed for JSON parsing into scope
 use serde::Deserialize;
 
+// Brings standard libraries needed for many things into scope
 use std::io::{self, BufRead};
 
 const CONFIG_FILE: &'static str = "config.cfg"; // Sets the name of the config file
 
+// Declares and intializes all the global variables used everywhere in the app
 lazy_static! {
     static ref HOME_NAME: Mutex<String> = Mutex::new(String::from("team_name"));
     static ref AWAY_NAME: Mutex<String> = Mutex::new(String::from("team_name"));
@@ -95,6 +101,7 @@ async fn main() {
         .route("/show_quarter_css", put(show_quarter_css_handler))
         // Routes for the file upload
         .route("/logo_upload", post(logo_upload_handler))
+        // Routes head requests for calculating latency
         .route("/ping", head(|| async { StatusCode::OK }))
         // Route the 404 page
         .fallback_service(get(|| async { 
@@ -107,18 +114,23 @@ async fn main() {
 
     // endregion: --- Routing
 
+    // Starts the clock ticker
     tokio::spawn(clock_ticker());
+    // Opens the config (or creates it if it doesnt exist) file and load configurations
     tokio::spawn(read_or_create_config()).await.unwrap();
 
+    // Gets address from the ADDR mutex
     let listen_addr = ADDR.lock().unwrap();
-
     let listen_addr: String = listen_addr.clone();
-
+    
+    // Bind the server to the address
     println!("Listening on: {}\nType \"stop\" to do shut down the server gracefully\n", listen_addr);
     let listener = tokio::net::TcpListener::bind(listen_addr).await.unwrap(); // Binds the listener to the address
-
+    
+    // Creates a oneshot channel to be able to shut down the server gracefully
     let (tx, rx) = tokio::sync::oneshot::channel();
-
+    
+    // Spawns a task to listen for the "stop" command which shuts down the server
     tokio::spawn(async move {
         let stdin = io::stdin();
         for line in stdin.lock().lines() {
@@ -129,12 +141,14 @@ async fn main() {
         }
     });
 
+    // Start the server
     let server = axum::serve(listener, app)
         .with_graceful_shutdown(async {
             let _ = rx.await;
             println!(" -> SERVER: shutting down");
         });
 
+    // Prints an error if an error occurs whie starting the server
     if let Err(err) = server.await {
         eprintln!(" -> ERROR: {}", err);
     }
@@ -143,7 +157,9 @@ async fn main() {
 
 // region: --- Config fn's
 
+// Function that creates and loads configurations from the config file
 async fn read_or_create_config() {
+    // Opens or creates the config file if it doesnt exist
     let config = match tokio::fs::read_to_string(CONFIG_FILE).await {
         Ok(cfg) => cfg,
         Err(_) => {
@@ -158,6 +174,7 @@ async fn read_or_create_config() {
         }
     };
 
+    // Split up the config file into lines and filter out comments
     let lines: Vec<String> = config
         .split('\n')
         .filter(|x| !x.starts_with("#"))
@@ -165,6 +182,7 @@ async fn read_or_create_config() {
         .collect();
     println!(" -> CONFIG: {:?}", lines);
 
+    // Loops through the lines and sets the configurations
     for i in lines {
         let parts: Vec<&str> = i.split('=').collect();
         match parts[0] {
@@ -188,21 +206,25 @@ async fn read_or_create_config() {
 // endregion: --- Config fn's
 // region: --- Page handlers
 
+// Serves the index.html file
 async fn idx_handler() -> Html<&'static str> {
     println!(" -> SERVE: index.html");
     Html(include_str!("html/index.html")) // Serves the contents of index.html
 }
 
+// Serves the chromakey.html file
 async fn chroma_handler() -> Html<&'static str> {
     println!(" -> SERVE: chromakey.html");
     Html(include_str!("html/scoreboard/chromakey.html"))
 }
 
+// Serve the upload.html file
 async fn upload_page_handler() -> Html<&'static str> {
     println!(" -> SERVE: upload.html");
     Html(include_str!("html/logo_upload/upload.html"))
 }
 
+// Serves the main css file
 async fn css_handler() -> impl IntoResponse {
     println!(" -> SERVE: style.css");
     let body = include_str!("html/style.css");
@@ -213,6 +235,7 @@ async fn css_handler() -> impl IntoResponse {
         .unwrap()
 }
 
+// Serves the htmx library
 async fn htmx_handler() -> impl IntoResponse {
     println!(" -> SERVE: htmx.min.js");
     let body = include_str!("html/htmx.min.js");
@@ -226,12 +249,14 @@ async fn htmx_handler() -> impl IntoResponse {
 // endregion: --- Page handlers
 // region: --- Team names
 
+// Struct to hold the team names
 #[derive(Deserialize)]
 struct UpdNames {
     home: String,
     away: String,
 }
 
+// Handles the form to update the team names
 async fn tname_handler(Form(names): Form<UpdNames>) {
     println!(" -> TEAMS: update names: {} - {}", names.home, names.away);
     let mut home_name = HOME_NAME.lock().unwrap();
@@ -240,26 +265,31 @@ async fn tname_handler(Form(names): Form<UpdNames>) {
     *away_name = names.away;
 }
 
+// Handles the display of the home team's name
 async fn hdisp_handler() -> Html<String> {
     let home_name = HOME_NAME.lock().unwrap();
     Html(format!("<h2>Home: {}</h2>", home_name))
 }
 
+// Handles the display of the away team's name
 async fn adisp_handler() -> Html<String> {
     let away_name = AWAY_NAME.lock().unwrap();
     Html(format!("<h2>Away: {}</h2>", away_name))
 }
 
+// Handles the display of the home team's name for the scoreboard
 async fn hname_scoreboard_handler() -> Html<String> {
     let home_name = HOME_NAME.lock().unwrap();
     Html(format!("{}", home_name))
 }
 
+// Handles the display of the away team's name for the scoreboard
 async fn aname_scoreboard_handler() -> Html<String> {
     let away_name = AWAY_NAME.lock().unwrap();
     Html(format!("{}", away_name))
 }
 
+// Handles and returns requests for the home team's logo
 async fn home_img_handler() -> impl IntoResponse {
     let home_image = tokio::fs::read(Path::new("home.png")).await.unwrap();
     let body = Body::from(home_image);
@@ -269,6 +299,7 @@ async fn home_img_handler() -> impl IntoResponse {
         .unwrap()
 }
 
+// Handles and returns requests for the away team's logo
 async fn away_img_handler() -> impl IntoResponse {
     let away_image = tokio::fs::read(Path::new("away.png")).await.unwrap();
     let body = Body::from(away_image);
@@ -281,12 +312,14 @@ async fn away_img_handler() -> impl IntoResponse {
 // endregion: --- Team names
 // region: --- Home handlers
 
+// Increases the home team's points by 1
 async fn hu_handler() {
     // Increments home points
     let mut home_points = HOME_POINTS.lock().unwrap();
     *home_points += 1;
 }
 
+// Decreases the home team's points by 1
 async fn hd_handler() {
     // Decrements home points
     let mut home_points = HOME_POINTS.lock().unwrap();
@@ -295,18 +328,21 @@ async fn hd_handler() {
     }
 }
 
+// Increases the home team's points by 2
 async fn hu2_handler() {
     // Adds 2 home points
     let mut home_points = HOME_POINTS.lock().unwrap();
     *home_points += 2;
 }
 
+// Increases the home team's points by 3
 async fn hu3_handler() {
     // Adds 3 home points
     let mut home_points = HOME_POINTS.lock().unwrap();
     *home_points += 3;
 }
 
+// Handles and returns the home team's points
 async fn hp_handler() -> Html<String> {
     // Displays home points
     let home_points = HOME_POINTS.lock().unwrap();
@@ -316,12 +352,14 @@ async fn hp_handler() -> Html<String> {
 // endregion: --- Home handlers
 // region: --- Away handlers
 
+// Increases the away team's points by 1
 async fn au_handler() {
     // Increments home points
     let mut away_points = AWAY_POINTS.lock().unwrap();
     *away_points += 1;
 }
 
+// Decreases the away team's points by 1
 async fn ad_handler() {
     // Decrements home points
     let mut away_points = AWAY_POINTS.lock().unwrap();
@@ -330,18 +368,21 @@ async fn ad_handler() {
     }
 }
 
+// Increases the away team's points by 2
 async fn au2_handler() {
     // Adds 2 home points
     let mut away_points = AWAY_POINTS.lock().unwrap();
     *away_points += 2;
 }
 
+// Increases the away team's points by 3
 async fn au3_handler() {
     // Adds 3 home points
     let mut away_points = AWAY_POINTS.lock().unwrap();
     *away_points += 3;
 }
 
+// Handles and returns the away team's points
 async fn ap_handler() -> Html<String> {
     // Displays home points
     let away_points = AWAY_POINTS.lock().unwrap();
@@ -351,6 +392,7 @@ async fn ap_handler() -> Html<String> {
 // endregion: --- Away Handlers
 // region: --- Clock handlers
 
+// Sets the clock to 8 minutes
 async fn quick_time8_handler() {
     let mut time_mins = TIME_MINS.lock().unwrap();
     let mut time_secs = TIME_SECS.lock().unwrap();
@@ -358,6 +400,7 @@ async fn quick_time8_handler() {
     *time_secs = 0;
 }
 
+// Sets the clock to 5 minutes
 async fn quick_time5_handler() {
     let mut time_mins = TIME_MINS.lock().unwrap();
     let mut time_secs = TIME_SECS.lock().unwrap();
@@ -365,6 +408,7 @@ async fn quick_time5_handler() {
     *time_secs = 0;
 }
 
+// Sets the clock to 3 minutes
 async fn quick_time3_handler() {
     let mut time_mins = TIME_MINS.lock().unwrap();
     let mut time_secs = TIME_SECS.lock().unwrap();
@@ -372,6 +416,7 @@ async fn quick_time3_handler() {
     *time_secs = 0;
 }
 
+// Sets the clock to 1 minute
 async fn quick_time1_handler() {
     let mut time_mins = TIME_MINS.lock().unwrap();
     let mut time_secs = TIME_SECS.lock().unwrap();
@@ -379,27 +424,32 @@ async fn quick_time1_handler() {
     *time_secs = 0;
 }
 
+// Handles and returns the time formatted as "mm:ss"
 async fn time_handler() -> Html<String> {
     let time_mins = TIME_MINS.lock().unwrap();
     let time_secs = TIME_SECS.lock().unwrap();
     Html(format!("{}:{:02?}", *time_mins, *time_secs))
 }
 
+// Handles and returns the minutes of the time
 async fn mins_handler() -> Html<String> {
     let time_mins = TIME_MINS.lock().unwrap();
     Html(format!("{}", *time_mins))
 }
 
+// Handles and returns the seconds of the time
 async fn secs_handler() -> Html<String> {
     let time_secs = TIME_SECS.lock().unwrap();
     Html(format!("{:02?}", *time_secs))
 }
 
+// Increases the minutes of the time by 1
 async fn mins_up_handler() {
     let mut time_mins = TIME_MINS.lock().unwrap();
     *time_mins += 1;
 }
 
+// Decreases the minutes of the time by 1
 async fn mins_down_handler() {
     let mut time_mins = TIME_MINS.lock().unwrap();
     if *time_mins > 0 {
@@ -407,6 +457,7 @@ async fn mins_down_handler() {
     }
 }
 
+// Increases the seconds of the time by 1
 async fn secs_up_handler() {
     let mut time_secs = TIME_SECS.lock().unwrap();
     if *time_secs < 59 {
@@ -414,6 +465,7 @@ async fn secs_up_handler() {
     }
 }
 
+// Decreases the seconds of the time by 1
 async fn secs_down_handler() {
     let mut time_secs = TIME_SECS.lock().unwrap();
     if *time_secs > 0 {
@@ -421,6 +473,7 @@ async fn secs_down_handler() {
     }
 }
 
+// Ticks the clock down if the clock is not stopped
 async fn clock_ticker() {
     loop {
         tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
@@ -442,12 +495,14 @@ async fn clock_ticker() {
     }
 }
 
+// Starts the clock
 async fn tstart_handler() {
     println!(" -> TIMER: start");
     let mut time_started = TIME_STARTED.lock().unwrap();
     *time_started = true;
 }
 
+// Stops the clock
 async fn tstop_handler() {
     println!(" -> TIMER: stop");
     let mut time_started = TIME_STARTED.lock().unwrap();
@@ -457,6 +512,7 @@ async fn tstop_handler() {
 // endregion: --- Clock handlers
 // region: --- Quarter handlers
 
+// Handles and returns the current quarter formatted for the scoreboard
 async fn quarter_handler() -> Html<&'static str> {
     let quarter = QUARTER.lock().unwrap();
     if *SHOW_QUARTER.lock().unwrap() {
@@ -476,6 +532,7 @@ async fn quarter_handler() -> Html<&'static str> {
     }
 }
 
+// Handles the show quarter button
 async fn quarter_show_handler() {
     let mut show_quarter = SHOW_QUARTER.lock().unwrap();
     if *show_quarter {
@@ -485,6 +542,7 @@ async fn quarter_show_handler() {
     }
 }
 
+// Handles and returns the css for the show quarter button
 async fn show_quarter_css_handler() -> Html<&'static str> {
     let show_quarter = SHOW_QUARTER.lock().unwrap();
     if *show_quarter {
@@ -494,21 +552,25 @@ async fn show_quarter_css_handler() -> Html<&'static str> {
     }
 }
 
+// Changes the quarter to 1
 async fn quarter1_change() {
     let mut quarter = QUARTER.lock().unwrap();
     *quarter = 1;
 }
 
+// Changes the quarter to 2
 async fn quarter2_change() {
     let mut quarter = QUARTER.lock().unwrap();
     *quarter = 2;
 }
 
+// Changes the quarter to 3
 async fn quarter3_change() {
     let mut quarter = QUARTER.lock().unwrap();
     *quarter = 3;
 }
 
+// Changes the quarter to 4
 async fn quarter4_change() {
     let mut quarter = QUARTER.lock().unwrap();
     *quarter = 4;
@@ -517,28 +579,37 @@ async fn quarter4_change() {
 // endregion: --- Quarter handlers
 // region: --- File upload handlers
 
+// Handles the file upload for the team's logo
 async fn logo_upload_handler(mut payload: Multipart) -> impl IntoResponse {
+    // Loops through the fields of the form
     while let Some(field) = payload.next_field().await.unwrap() {
+        // Gets the name and data of the field
         let name = field.name().unwrap().to_string();
         let data = field.bytes().await.unwrap();
 
+        // Writes the data to a .png file
         println!(" -> LOGO: recieved {}\n\tLENGTH: {}", name, data.len());
         tokio::fs::write(Path::new(&name), data).await.unwrap();
 
+        // Opens the image and gets its dimension
         let img = image::open(&name).unwrap();
         let img_dimensions = image::image_dimensions(&name).unwrap();
 
         println!(" -> IMG: {:?}", img_dimensions);
 
+        // Gets width and height as a float
         let height = img_dimensions.1 as f32;
         let width = img_dimensions.0 as f32;
 
+        // Finds the ratio to resize the image to 30px height
         let resize_ratio = 30.0 / height;
         println!(" -> RESIZE: {}%", resize_ratio * 100.0);
 
+        // Finds new image dimensions
         let height: u32 = (height * resize_ratio) as u32;
         let width: u32 = (width * resize_ratio) as u32;
 
+        // Resizes and saves the resized image
         println!(" -> RESIZE {}x{}", height, width);
         let resized =
             image::imageops::resize(&img, width, height, image::imageops::FilterType::Lanczos3);
@@ -554,10 +625,12 @@ async fn logo_upload_handler(mut payload: Multipart) -> impl IntoResponse {
 // endregion: --- File upload handlers
 // region: --- Misc handelers
 
+// Function for testing http requests
 //async fn test_handler() {
 //    println!(" -> TEST: test");
 //}
 
+// Handles and returns the chromakey color as a css background color
 async fn chromargb_handler() -> Html<String> {
     let chromakey = CHROMAKEY.lock().unwrap();
     Html(format!(
@@ -566,12 +639,14 @@ async fn chromargb_handler() -> Html<String> {
     ))
 }
 
+// Handles and returns the score as a string formatted for the scoreboard
 async fn score_handler() -> Html<String> {
     let home_points = HOME_POINTS.lock().unwrap();
     let away_points = AWAY_POINTS.lock().unwrap();
     Html(format!("{} - {}", home_points, away_points))
 }
 
+// Handles and returns the time and quarter as a string formatted for the scoreboard
 async fn time_and_quarter_handler() -> Html<String> {
     let time_mins = TIME_MINS.lock().unwrap();
     let time_secs = TIME_SECS.lock().unwrap();
