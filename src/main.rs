039f21299358be123548f2d751c728ec8d6db9f1
AllLiver <oliverfedd@outlook.com>
2024-10-14 10:24:04 -0600

    Self updating 2/3 (update menu in settings)

--- src/main.rs
+++ src/main.rs
@@ -61,6 +61,8 @@ lazy_static! {
         Arc::new(Mutex::new(None));
     static ref UPDATE_SIGNAL: Arc<Mutex<Option<oneshot::Sender<()>>>> = Arc::new(Mutex::new(None));
     static ref EXIT_CODE: Arc<Mutex<Option<i32>>> = Arc::new(Mutex::new(None));
+    static ref OUT_OF_DATE: Arc<Mutex<bool>> = Arc::new(Mutex::new(false));
+    static ref REMOTE_VERSION: Arc<Mutex<String>> = Arc::new(Mutex::new(String::from("0.0.0")));
 }
 
 #[macro_export]
@@ -383,6 +385,7 @@ async fn main() -> Result<()> {
             "/version",
             put(|| async { Html::from(env!("CARGO_PKG_VERSION")) }),
         )
+        .route("/update/menu", put(update_menu_handler))
         .nest("/", auth_session_routes)
         .nest("/", auth_give_session_routes)
         .with_state(state.clone())
@@ -397,6 +400,7 @@ async fn main() -> Result<()> {
         tokio::spawn(sponsor_ticker());
         tokio::spawn(popup_home_ticker());
         tokio::spawn(popup_away_ticker());
+        tokio::spawn(auto_update_checker());
         printlg!(" -> LISTENING ON: 0.0.0.0:3000");
 
         axum::serve(listener, app)
@@ -2485,6 +2489,105 @@ async fn api_key_reveal_handler() -> impl IntoResponse {
 }
 
 // endregion: api
+// region: updating
+
+const REMOTE_CARGO_TOML_URL: &'static str =
+    "https://raw.githubusercontent.com/AllLiver/Froggi/refs/heads/main/Cargo.toml";
+
+async fn update_handler() -> impl IntoResponse {
+    printlg!("Checking if an update is availible...");
+
+    if let Ok(result) = update_checker().await {
+        if result.0 {
+            if let Some(tx) = UPDATE_SIGNAL.lock().await.take() {
+                printlg!("Starting update...");
+                let _ = tx.send(());
+                return Response::builder()
+                    .status(StatusCode::OK)
+                    .body(String::from("Starting update..."))
+                    .unwrap();
+            } else {
+                printlg!("Update signal already sent");
+                return Response::builder()
+                    .status(StatusCode::CONFLICT)
+                    .body(String::from("Update already sent!"))
+                    .unwrap();
+            }
+        } else {
+            printlg!("Already up to date!");
+            return Response::builder()
+                .status(StatusCode::METHOD_NOT_ALLOWED)
+                .body(String::from("Already up to date!"))
+                .unwrap();
+        }
+    } else {
+        printlg!("Failed to make request to {}", REMOTE_CARGO_TOML_URL);
+        return Response::builder()
+            .status(StatusCode::SERVICE_UNAVAILABLE)
+            .body(format!(
+                "Failed to make request to {}",
+                REMOTE_CARGO_TOML_URL
+            ))
+            .unwrap();
+    }
+}
+
+async fn update_checker() -> Result<(bool, String)> {
+    let result = reqwest::get(REMOTE_CARGO_TOML_URL).await;
+
+    if let Ok(response) = result {
+        let remote_version_str_raw = response.text().await.expect("Failed to get response text");
+        let remote_version_str = remote_version_str_raw
+            .split("\n")
+            .collect::<Vec<&str>>()
+            .iter()
+            .find(|x| x.starts_with("version = "))
+            .expect("Failed to get remote version")
+            .trim_start_matches("version = \"")
+            .trim_end_matches("\"");
+
+        let local_version_str = env!("CARGO_PKG_VERSION");
+
+        let remote_version: Vec<u8> = remote_version_str
+            .split(".")
+            .map(|x| x.parse::<u8>().expect("Failed to parse remote version"))
+            .collect();
+        let local_version: Vec<u8> = local_version_str
+            .split(".")
+            .map(|x| x.parse::<u8>().expect("Failed to parse remote version"))
+            .collect();
+
+        let mut out_of_date = false;
+
+        for i in 0..local_version.len() {
+            if remote_version[i] > local_version[i] {
+                out_of_date = true;
+                break;
+            } else if remote_version[i] < local_version[i] {
+                break;
+            }
+        }
+
+        return Ok((out_of_date, String::from(remote_version_str)));
+    } else if let Err(e) = result {
+        return Err(anyhow!("{}", e));
+    } else {
+        return Err(anyhow!("Some unexpected error"));
+    }
+}
+
+async fn auto_update_checker() {
+    loop {
+        if let Ok(r) = update_checker().await {
+            *OUT_OF_DATE.lock().await = r.0;
+            *REMOTE_VERSION.lock().await = r.1;
+        }
+
+        tokio::time::sleep(std::time::Duration::from_secs(60 * 10)).await;
+    }
+}
+
+// endregion: updating
 // region: popups
 
 async fn popup_handler(
@@ -2708,76 +2811,22 @@ async fn shutdown_handler() -> impl IntoResponse {
     }
 }
 
-const REMOTE_CARGO_TOML_URL: &'static str =
-    "https://github.com/AllLiver/Froggi/blob/main/Cargo.toml";
-
-async fn update_handler() -> impl IntoResponse {
-    printlg!("Checking if an update is availible...");
-
-    if let Ok(response) = reqwest::get(REMOTE_CARGO_TOML_URL).await {
-        let remote_version_str_raw = response.text().await.expect("Failed to get response text");
-        let remote_version_str = remote_version_str_raw
-            .split("\n")
-            .collect::<Vec<&str>>()
-            .iter()
-            .find(|x| x.starts_with("version = "))
-            .expect("Failed to get remote version")
-            .trim_start_matches("version = \"")
-            .trim_end_matches("\"");
-
-        let local_version_str = env!("CARGO_PKG_VERSION");
-
-        let remote_version: Vec<u8> = remote_version_str
-            .split(".")
-            .map(|x| x.parse::<u8>().expect("Failed to parse remote version"))
-            .collect();
-        let local_version: Vec<u8> = local_version_str
-            .split(".")
-            .map(|x| x.parse::<u8>().expect("Failed to parse remote version"))
-            .collect();
-
-        let mut out_of_date = false;
-
-        for i in 0..local_version.len() {
-            if remote_version[i] > local_version[i] {
-                out_of_date = true;
-                break;
-            } else if remote_version[i] < local_version[i] {
-                break;
-            }
-        }
-
-        if out_of_date {
-            if let Some(tx) = UPDATE_SIGNAL.lock().await.take() {
-                printlg!("Starting update...");
-                let _ = tx.send(());
-                return Response::builder()
-                    .status(StatusCode::OK)
-                    .body(String::from("Starting update..."))
-                    .unwrap();
-            } else {
-                printlg!("Update signal already sent");
-                return Response::builder()
-                    .status(StatusCode::CONFLICT)
-                    .body(String::from("Update already sent!"))
-                    .unwrap();
-            }
-        } else {
-            printlg!("Already up to date!");
-            return Response::builder()
-                .status(StatusCode::METHOD_NOT_ALLOWED)
-                .body(String::from("Already up to date!"))
-                .unwrap();
-        }
+async fn update_menu_handler() -> impl IntoResponse {
+    if *OUT_OF_DATE.lock().await {
+        return Html::from(format!("
+        <div class=\"update-menu\">
+            <h2>Update</h2>
+            <h3>Update availible! ({} -> {})</h3>
+            <br>
+            <p>Do not restart the server during update!</p>
+            <button hx-post=\"/update\" hx-confirm=\"Are you sure would like to update Froggi? This completely stops Froggi until the update is complete.\">Update now</button>
+        </div>
+        ", 
+        env!("CARGO_PKG_VERSION"),
+        *REMOTE_VERSION.lock().await
+    ));
     } else {
-        printlg!("Failed to make request to {}", REMOTE_CARGO_TOML_URL);
-        return Response::builder()
-            .status(StatusCode::SERVICE_UNAVAILABLE)
-            .body(format!(
-                "Failed to make request to {}",
-                REMOTE_CARGO_TOML_URL
-            ))
-            .unwrap();
+        return Html::from(String::from("<div class=\"update-menu\"><h2>Update</h2><h3>Froggi is up to date!</h3></div>"));
     }
 }
 
