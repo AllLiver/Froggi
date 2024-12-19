041ee2ad6a8c7205cec05d1ae79ea47cff603dbb
mindelou <90578367+mindelou@users.noreply.github.com>
2024-12-18 22:22:43 -0700

    various bug

--- src/routing/teaminfo.rs
+++ src/routing/teaminfo.rs
@@ -178,11 +178,11 @@ pub async fn teaminfo_preset_selector_handler() -> impl IntoResponse {
             html += &format!(
             "<div class=\"ti-match-selector\">
                 <img class=\"home-logo\" src=\"data:image/{};base64,{}\" alt=\"home-img\" height=\"30px\" width=\"auto\" style=\"border-color: {}; border-style: solid; border-radius: 3px; border-width: 2px\">
-                <p class=\"teampreset-title\">{} vs {}</p>
+                <p class=\"ti-teampreset-title\">{} vs {}</p>
                 <img class=\"away-logo\" src=\"data:image/{};base64,{}\" alt=\"away-img\" height=\"30px\" width=\"auto\" style=\"border-color: {}; border-style: solid; border-radius: 3px; border-width: 2px;\">
-                <button class=\"select-button\" hx-post=\"/teaminfo/select/{}\" hx-swap=\"none\">Select</button>
-                <button class=\"remove-button\" hx-post=\"/teaminfo/remove/{}\" hx-swap=\"none\">Remove</button>
-                <a href=\"/teaminfo/download-preset/{}\">Download</button>
+                <button class=\"ti-select-button\" hx-post=\"/teaminfo/select/{}\" hx-swap=\"none\">Select</button>
+                <button class=\"ti-remove-button\" hx-post=\"/teaminfo/remove/{}\" hx-swap=\"none\">Remove</button>
+                <a class=\"ti-download-button\" href=\"/teaminfo/download-preset/{}\">Download</button>
             </div>",
                 home_tag_type,
                 BASE64_STANDARD.encode(home_img_bytes),
